8dd5ae7696559be2b01f5d0795f8d63c499bf612
bhoomikasuresh15 <bhoomikasuresh200715@gmail.com>
2026-08-22 13:28:12 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,58 @@
+public class User {
+
+    private int id;
+    private String employeeId;
+    private String email;
+    private String password;
+    private String role;
+
+    public User(int id, String employeeId, String email,
+                String password, String role) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
+ṇ22ṇ
